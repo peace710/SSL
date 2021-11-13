8f4a9bfd2b11420fcd5f738b15c9842c4f30399d
peace710 <wxxinyun@163.com>
2021-11-13 17:17:20 +0800

    format code

--- linux/test/ioutil/io_util.cpp
+++ linux/test/ioutil/io_util.cpp
@@ -2,9 +2,9 @@
 #include "io_util.h"
 
 int printHex(unsigned const char* data,int len){
-   for (int i = 0 ; i < len ;i++){
-   	printf("%X",data[i]);
-   }
-   printf("\n");
-   return 0;
+	for (int i = 0 ; i < len ;i++){
+		printf("%X",data[i]);
+   	}
+   	printf("\n");
+   	return 0;
 }
--- linux/test/md5/main.cpp
+++ linux/test/md5/main.cpp
@@ -4,20 +4,20 @@
 #include <io_util.h>
 
 int main(int argc,char* argv[]){
-    char data[]="hello world";
-    uint8_t out[MD5_DIGEST_LENGTH];
+	char data[]="hello world";
+   	uint8_t out[MD5_DIGEST_LENGTH];
     
-    MD5_CTX md5;
-    MD5_Init(&md5);
-    MD5_Update(&md5,data,strlen(data));
-    MD5_Final(out,&md5);  
-    printf("hello world md5:\n");
-    printHex((unsigned const char*)out,MD5_DIGEST_LENGTH);
+    	MD5_CTX md5;
+    	MD5_Init(&md5);
+    	MD5_Update(&md5,data,strlen(data));
+    	MD5_Final(out,&md5);  
+    	printf("hello world md5:\n");
+    	printHex((unsigned const char*)out,MD5_DIGEST_LENGTH);
     
-    uint8_t easyout[MD5_DIGEST_LENGTH];
-    MD5((const uint8_t*)data,strlen(data),easyout);
-    printf("hello world md5(easy):\n");
-    printHex((unsigned const char*)easyout,MD5_DIGEST_LENGTH);
+    	uint8_t easyout[MD5_DIGEST_LENGTH];
+    	MD5((const uint8_t*)data,strlen(data),easyout);
+    	printf("hello world md5(easy):\n");
+    	printHex((unsigned const char*)easyout,MD5_DIGEST_LENGTH);
 
-    return 0;
+    	return 0;
 }
